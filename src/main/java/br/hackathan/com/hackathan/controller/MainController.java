a838f7aa0f9d03b8db9b62e5880d42d8ff2ede1c
yThanos <vitorfraporti@hotmail.com>
2024-04-19 20:02:19 -0300

    a

--- src/main/java/br/hackathan/com/hackathan/controller/MainController.java
+++ src/main/java/br/hackathan/com/hackathan/controller/MainController.java
@@ -4,20 +4,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.hackathan.com.hackathan.model.Item;
+import br.hackathan.com.hackathan.model.PontoAcesso;
 import br.hackathan.com.hackathan.repository.ItemRepository;
+import br.hackathan.com.hackathan.repository.PontoRepository;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RestController
 @RequestMapping("/main")
 public class MainController {
 
     private final ItemRepository itemRepository;
+    private final PontoRepository pontoRepository;
 
-    public MainController(ItemRepository itemRepository) {
+    public MainController(ItemRepository itemRepository, PontoRepository pontoRepository) {
+        this.pontoRepository = pontoRepository;
         this.itemRepository = itemRepository;
     }
     
@@ -27,5 +30,9 @@ public class MainController {
         System.out.println("Item example: "+itens.get(0));
         return itens;
     }
-    
+
+    @GetMapping("/pontos")
+    public List<PontoAcesso> ponto() {
+        return pontoRepository.findAll();
+    }
 }
