176395e49c0a80f2361554c70bf5773ae112a56b
yThanos <vitorfraporti@hotmail.com>
2024-04-18 22:50:19 -0300

    aaa

--- src/main/java/br/hackathan/com/hackathan/controller/AuthController.java
+++ src/main/java/br/hackathan/com/hackathan/controller/AuthController.java
@@ -3,9 +3,14 @@ package br.hackathan.com.hackathan.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.hackathan.com.hackathan.Util.JwtUtil;
 import br.hackathan.com.hackathan.model.Usuario;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,11 +18,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
     
     @PostMapping("path")
     public ResponseEntity<Object> login(@RequestBody Usuario user) {
-        
-        return ResponseEntity.ok().build();
+        final Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(user.getEmail(), user.getSenha())
+        );
+        if(authentication.isAuthenticated()) {
+            String token = JwtUtil.generateToken(user.getEmail());
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Credenciais inválidas", HttpStatus.UNAUTHORIZED);
     }
     
 }
--- src/main/java/br/hackathan/com/hackathan/security/CustomUserDetailsService.java
+++ src/main/java/br/hackathan/com/hackathan/security/CustomUserDetailsService.java
@@ -5,7 +5,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
